79f32edc1ae519ec3a3525230d50452db5a556df
fabri10roman <fabri11roman.fr@gmail.com>
2024-01-03 22:38:21 -0300

    init

---
+++ src/main/java/com/example/ContaGest/ContaGestApplication.java
@@ -0,0 +1,13 @@
+package com.example.ContaGest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ContaGestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ContaGestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/ContaGest/ContaGestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ContaGest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ContaGestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
